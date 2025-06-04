171309cb35ecc6d0df2dc5340f1192973e0f27ac
LA <75972079+auritot@users.noreply.github.com>
2025-06-04 13:53:56 +0800

    Update asgi.py

--- neuroforum_django/asgi.py
+++ neuroforum_django/asgi.py
@@ -8,14 +8,19 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 """
 
 import os
+
+# 1. Set the DJANGO_SETTINGS_MODULE before any Django/Channels imports
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "neuroforum_django.settings")
+
+# 2. Initialize Django
 import django
+django.setup()
+
+# 3. Now import Channels and your middleware/router
 from channels.routing import ProtocolTypeRouter, URLRouter
 from forum.middleware import SessionAuthMiddleware
 from django.core.asgi import get_asgi_application
 import forum.routing
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "neuroforum_django.settings")
-django.setup()
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
