9a4b76c697f14688ee5d52ec2663957bc512c12a
LA <75972079+auritot@users.noreply.github.com>
2025-06-08 17:51:37 +0800

    still fixing csp

--- forum/templates/static/js/chat_init.js
+++ forum/templates/static/js/chat_init.js
@@ -1,28 +1,28 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const chatBtn = document.getElementById("chat-btn");
-    const chatBox = document.getElementById("chat-box-floating");
-    const closeChat = document.getElementById("close-chat");
-    const chatFrame = document.getElementById("chat-frame");
+  const chatBtn = document.getElementById("chat-btn");
+  const chatBox = document.getElementById("chat-box-floating");
+  const closeChat = document.getElementById("close-chat");
+  const chatFrame = document.getElementById("chat-frame");
 
-    if (chatBtn) {
-        chatBtn.addEventListener("click", () => {
-                if (username) {
-                    chatFrame.src = "{% url 'chat_landing' %}?frame=1";
-                    chatBox.classList.remove("d-none");
-                } else {
-                    alert("Please log in to use chat.");
-                }
+  const username = window.currentUsername;
 
-        });
-    }
-
-    closeChat.addEventListener("click", () => {
-        chatBox.classList.add("d-none");
-        chatFrame.src = "";
+  if (chatBtn) {
+    chatBtn.addEventListener("click", () => {
+      if (username) {
+        chatFrame.src = "/chat/landing/?frame=1";
+        chatBox.classList.remove("d-none");
+      } else {
+        alert("Please log in to use chat.");
+      }
     });
+  }
 
-    chatFrame.addEventListener("load", () => {
-        chatFrame.classList.remove("loading");
-    });
+  closeChat.addEventListener("click", () => {
+    chatBox.classList.add("d-none");
+    chatFrame.src = "";
+  });
 
+  chatFrame.addEventListener("load", () => {
+    chatFrame.classList.remove("loading");
+  });
 });
