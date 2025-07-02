a88e68e7451fb01550a795929796e935f477e15a
LA <75972079+auritot@users.noreply.github.com>
2025-07-02 21:32:07 +0800

    Update middleware.py

--- forum/middleware.py
+++ forum/middleware.py
@@ -58,7 +58,7 @@ class SessionAuthMiddleware(BaseMiddleware):
 
 class IPBanMiddleware(MiddlewareMixin):
     def process_request(self, request):
-        ip = self.get_client_ip(request)
+        ip = get_client_ip(request)
         print(f"👀 Middleware sees IP: {ip}")
         print(f"🚫 Ban exists? {cache.get(f'login_ban_{ip}')}")
 
